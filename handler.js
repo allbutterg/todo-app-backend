3d53c4262554ac71b18e43d6e32ef2e8d601477c
Gauri <gauri.kangai@outlook.com>
2020-02-23 13:07:39 +0000

    Resolved server success error

--- handler.js
+++ handler.js
@@ -4,7 +4,7 @@ const serverless = require('serverless-http');
 const express = require('express')
 const app = express()
 
-app.get('/tasks', function (request, response) {
+app.get('/tasks', function (req, res) {
   res.send('Hello World!')
 })
 
@@ -24,4 +24,4 @@ module.exports.tasks = serverless(app);
 
   // Use this code if you don't use the http event with the LAMBDA-PROXY integration
   // return { message: 'Go Serverless v1.0! Your function executed successfully!', event };
-};
+// };
