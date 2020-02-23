d5f308650fe379b676f07a4d050bd711a615adbe
Gauri <gauri.kangai@outlook.com>
2020-02-23 17:11:17 +0000

    Setting up configuration variables

--- handler.js
+++ handler.js
@@ -1,45 +1,63 @@
 'use strict';
 
 const serverless = require('serverless-http');
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const uuidv4 = require('uuid/v4');
+const mysql = require('mysql');
 
-app.get('/tasks', function (req, res) {
-  res.json ({
-   message: 
-   [
-    {
-        id: 1,
-        category: "Personal",
-        description: "Complete TR homework",
-        priority: 1,
-        goaldate: "19/03/20",
-        completed: false
-    },
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_SCHEMA
+});
 
-    {
-        id: 2,
-        category: "Personal",
-        description: "Renew Driving Licence",
-        priority: 1,
-        goaldate: "16/02/20",
-        completed: false
-    },
+// Retrieving tasks
 
-    {
-        id: 3,
-        category: "Personal",
-        description: "Book theatre tickets",
-        priority: 2,
-        goaldate: "29/02/20",
-        completed: false
+app.get('/task', function (req, res) {
+
+  connection.query('SELECT * FROM `task` WHERE `categoryId` = "2"', function (error, results, fields) {
+    // error will be an Error if one occurred during the query
+    if (error) {
+      console.error("Your query had a problem with retrieving tasks", error);
+      res.status(500).json({ errorMessage: error });
     }
+    else {
+      // Query was successful
+      res.json({ tasks: results });
+    }
+    // results will contain the results of the query
+    // fields will contain information about the returned results fields (if any)
+  });
 
-]
-});
 });
 
-module.exports.tasks = serverless(app);
+
+// Creating tasks
+app.post('/task', function (req, res) {
+  res.json({
+    message: 'Gauri, your POST works!',
+  });
+});
+
+// Updating tasks
+app.put('/task/:taskId', function (req, res) {
+  res.json({
+    message: 'Gauri, your PUT works!',
+  });
+});
+
+
+// Deleting tasks
+app.delete('/task/:taskId', function (req, res) {
+  res.json({
+    message: 'Gauri, your DELETE works!',
+  });
+});
+
+
+module.exports.task = serverless(app);
 
 // module.exports.tasks = async event => {
 //   return {
